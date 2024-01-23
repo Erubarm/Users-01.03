418d6f5a238ad78dbcb77d273b54397dad6dd3c6
Erubarm <kirill.komigachev@yandex.ru>
2024-01-23 14:33:18 +0300

    add search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 const itemsList = document.querySelector(".items-list");
 const buttonFilter = document.querySelector(".filtered");
-document.body.onload = () => {
-    setTimeout( () => {
-        const preloader = document.querySelector('.preloader');
-        if ( !preloader.classList.contains('hide')) {
-            preloader.classList.add('hide');
-        }
-    }, 1500);
-}
+const username = document.getElementById("username");
+const userFind = document.getElementById("userFind");
 
-const filterImg = document.querySelector('.filtered__img')
-console.log(filterImg.classList)
+document.body.onload = () => {
+  setTimeout(() => {
+    const preloader = document.querySelector(".preloader");
+    if (!preloader.classList.contains("hide")) {
+      preloader.classList.add("hide");
+    }
+  }, 1500);
+};
+
+const filterImg = document.querySelector(".filtered__img");
+console.log(filterImg.classList);
 const getUsers = async () => {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   return users.json();
@@ -33,6 +36,27 @@ const Actions = async () => {
     };
   });
 
+  const searchUser = (username) => {
+    const filters = filteredUsers.filter((user) => user.name.includes(username));
+    filters.forEach((user) => {
+    itemsList.innerHTML += `
+            <li>
+                <div class="user__avatar"></div>
+                <div class="user__description">
+                  <h2 class="user__name"><a href="./userProfile.html?userId=${user.userId}">${user.name}</a></h2>
+                  <span class="user__company normal">${user.company}</span>  
+                  <span class="user__email small">${user.email}</span>
+                </div>
+            </li>
+        `;
+    });
+  };
+
+  userFind.addEventListener("click", () => {
+    itemsList.innerHTML = ` `;
+    searchUser(username.value);
+  });
+
   const outputUsers = () => {
     filteredUsers.forEach((user) => {
       itemsList.innerHTML += `
@@ -47,16 +71,18 @@ const Actions = async () => {
         `;
     });
   };
+
+  
   outputUsers();
 
   buttonFilter.addEventListener("click", () => {
-    if (!filterImg.classList.contains('rotate') ) {
-      filterImg.classList.add('rotate')
+    if (!filterImg.classList.contains("rotate")) {
+      filterImg.classList.add("rotate");
       filteredUsers.sort((a, b) => a.name.localeCompare(b.name));
       itemsList.innerHTML = ``;
       outputUsers();
     } else {
-      filterImg.classList.remove('rotate');
+      filterImg.classList.remove("rotate");
       filteredUsers.reverse();
       itemsList.innerHTML = ``;
       outputUsers();
